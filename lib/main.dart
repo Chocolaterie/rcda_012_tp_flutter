f3c4a3a318d1e08ae9d73faab97323be3eae2383
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-07-30 16:57:34 +0200

    Correction tp 1 partie 3

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,22 @@ class HeaderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(gradient: LinearGradient(colors: [Color(0xFF3e60ac), Color(0xFF55a4e0)])),
+      decoration: BoxDecoration(
+          gradient:
+              LinearGradient(colors: [Color(0xFF3e60ac), Color(0xFF55a4e0)])),
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Text("Accueil", style: TextStyle(color: Colors.white),),
+            Text(
+              "Accueil",
+              style: TextStyle(color: Colors.white),
+            ),
             Text("Nouveau", style: TextStyle(color: Colors.white)),
             Text("Rechercher", style: TextStyle(color: Colors.white)),
-          ],),
+          ],
+        ),
       ),
     );
   }
@@ -45,8 +51,7 @@ class FooterWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xFFEEEEEE),
-      child:
-      Padding(
+      child: Padding(
         padding: const EdgeInsets.all(20),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -57,7 +62,8 @@ class FooterWidget extends StatelessWidget {
             Text("Moi"),
           ],
         ),
-      ),);
+      ),
+    );
   }
 }
 
@@ -70,11 +76,73 @@ class MyHomePage extends StatelessWidget {
         title: Text("Message"),
         centerTitle: true,
       ),
-      body: Column(children: [
-        HeaderWidget(),
-        Expanded(child: Container(child: Text("TODO"),)),
-        FooterWidget()
-      ],),
+      body: Column(
+        children: [
+          HeaderWidget(),
+          Expanded(
+              child: Column(
+            children: [
+              SizedBox(
+                height: 180,
+                child: Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Image.network(
+                            "https://avatar.iran.liara.run/public",
+                            width: 96,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "shiba@gmail.com",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text("56s")
+                                    ],
+                                  ),
+                                  Text(
+                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 3,
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text("Répondre"),
+                            Text("Retweet"),
+                            Text("Favoris")
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
+          FooterWidget()
+        ],
+      ),
     );
   }
 }
