d9b805fb163b548b2a559fb53eb80acc0acc11d7
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-07-31 15:13:26 +0200

    Correction

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rcda_012_tp_flutter/app-theme.dart';
+import 'package:rcda_012_tp_flutter/auth-page.dart';
 import 'package:rcda_012_tp_flutter/footer.dart';
 import 'package:rcda_012_tp_flutter/header.dart';
 import 'package:rcda_012_tp_flutter/message-card.dart';
@@ -20,8 +21,13 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
           appBarTheme: AppTheme.appBarTheme,
+
       ),
-      home: MyHomePage(),
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => AuthPage(),
+        "/messages" : (context) => MyHomePage()
+      },
     );
   }
 }
