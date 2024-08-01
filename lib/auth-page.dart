d4840689ce3e8a46b7c8c91d40f8151af301f6af
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-08-01 09:55:11 +0200

    Correction tp 3

--- lib/auth-page.dart
+++ lib/auth-page.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:rcda_012_tp_flutter/app-theme.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:rcda_012_tp_flutter/app-validator.dart';
 
 class AuthPage extends StatelessWidget {
+
+  // La clé du formulaire
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void onSubmit(BuildContext context) {
+    // Tester si formulaire valide
+    if (!_formKey.currentState!.validate()){
+      return;
+    }
+    // TODO : Appel service auth (test connexion et token)
+    // Changer de page
+    Navigator.pushNamed(context, "/messages");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,34 +31,43 @@ class AuthPage extends StatelessWidget {
           Image.asset("mobile_background.jpg", fit: BoxFit.cover,),
           Padding(
             padding: const EdgeInsets.all(60),
-            child: Column(
-              children: [
-                Image.asset("sign_in_icon.png", width: 126,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 20),
-                  child: Text("Login", style: TextStyle(color: Color(0xDDFFFFFF), fontSize: 38),),
-                ),
-                AppTheme.wrapFormPadding(
-                    TextField(decoration: AppTheme.fieldDecoration("Email", "Veuillez saisir un email"),)
-                ),
-                AppTheme.wrapFormPadding(
-                    TextField(decoration: AppTheme.fieldDecoration("Password", "Veuillez saisir un mot de passe"),)
-                ),
-                AppTheme.wrapFormPadding(
-                    Row(children: [
-                      Switch(value: false, onChanged: (value) {}),
-                      Text("Mémoriser mes informations", style: TextStyle(color: Color(0xFFFFFFFF)),)
-                    ],)
-                ),
-                AppTheme.wrapFormPadding(
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(onPressed: () { }, child: Padding(
-                        padding: const EdgeInsets.all(15.0),
-                          child: Text('Submit'),
-                        )))
-                ),
-              ],
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Image.asset("sign_in_icon.png", width: 126,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Text("Login", style: TextStyle(color: Color(0xDDFFFFFF), fontSize: 38),),
+                  ),
+                  AppTheme.wrapFormPadding(
+                      TextFormField(
+                        validator: AppValidator.emailValidator,
+                        style: TextStyle(color: Colors.white),
+                        decoration: AppTheme.fieldDecoration("Email", "Veuillez saisir un email"),)
+                  ),
+                  AppTheme.wrapFormPadding(
+                      TextFormField(
+                        validator: AppValidator.passwordValidator,
+                        style: TextStyle(color: Colors.white),
+                        decoration: AppTheme.fieldDecoration("Password", "Veuillez saisir un mot de passe"),)
+                  ),
+                  AppTheme.wrapFormPadding(
+                      Row(children: [
+                        Switch(value: false, onChanged: (value) {}),
+                        Text("Mémoriser mes informations", style: TextStyle(color: Color(0xFFFFFFFF)),)
+                      ],)
+                  ),
+                  AppTheme.wrapFormPadding(
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(onPressed: () { onSubmit(context); }, child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                            child: Text('Submit'),
+                          )))
+                  ),
+                ],
+              ),
             ),
           )
         ],
